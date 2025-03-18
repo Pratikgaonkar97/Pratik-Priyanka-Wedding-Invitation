e7b18f4309571ee3b76db36cf7d482cc9ec5423d
Pratikgaonkar97 <129770172+Pratikgaonkar97@users.noreply.github.com>
2025-03-18 15:37:11 +0530

    Update document.js

--- document.js
+++ document.js
@@ -24,4 +24,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const countdownInterval = setInterval(updateCountdown, 1000);
     updateCountdown();
+
+    document.addEventListener("DOMContentLoaded", function () {
+    const heartsContainer = document.querySelector(".hearts-container");
+
+    function createHeart() {
+        const heart = document.createElement("div");
+        heart.classList.add("heart");
+        heart.style.left = Math.random() * 100 + "vw";
+        heart.style.animationDuration = Math.random() * 2 + 3 + "s";
+        heartsContainer.appendChild(heart);
+
+        setTimeout(() => {
+            heart.remove();
+        }, 4000);
+    }
+
+    setInterval(createHeart, 500);
+});
+
 });
